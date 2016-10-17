40bc0f33c728d9373242fafc0a1c699219a6ecd5
vincentsarago <vincent.sarago@gmail.com>
2016-10-17 00:11:35 -0400

    added mailing for subscribe and unsubscribe

--- database/handler.js
+++ database/handler.js
@@ -1,6 +1,24 @@
 var mongodb = require('mongodb');
+var mark = require('markup-js');
+var aws = require('aws-sdk');
+var fs = require('fs');
+
 var config = require('./config');
 var mongodbUri = 'mongodb://' + config.mongodb.login + ':' + config.mongodb.password + '@' + config.mongodb.host + ':' + config.mongodb.port + '/ddb';
+
+var ses = new aws.SES({region: 'us-west-2'});
+
+function sendMail(content, callback) {
+    ses.sendEmail(content, function (err, data) {
+        if (err) {
+            console.log('Internal Error: The email could not be sent.');
+            return callback(null, 'Internal Error: The email could not be sent.');
+        } else {
+            console.log('The email was successfully sent to ' + content.Destination.ToAddresses[0]);
+            return callback(null, 'The email was successfully sent to ' + content.Destination.ToAddresses[0]);
+        }
+    });
+}
 
 //ADD (feature)
 module.exports.add = function(event, context) {
@@ -22,17 +40,46 @@ module.exports.add = function(event, context) {
             disaster.mail = [mail];
         }
 
-        col.insert(disaster, function(err, docs){
+        col.insert(disaster, function(err, res){
             if (err) {
                 console.log("Couldn't Add disasterEvent to database");
                 context.fail(err);
             }
             db.close();
-
-            //Confirmation MAIL to "mailTO"
-
             console.log("disasterEvent Added to database");
-            context.succeed("disasterEvent Added to database");
+
+            if (mail) {
+                var mailTemplate = fs.readFileSync('./templates/subscribe.html', 'utf8'),
+                    info = {
+                        name: disaster.feature.properties.name,
+                        uuid: disaster.feature.properties.uuid,
+                        mail: mail.mail
+                    },
+                    message = mark.up(mailTemplate, info),
+                    params = {
+                        Destination: {
+                            ToAddresses: [info.mail]
+                        },
+                        Message: {
+                            Body: {
+                                Html: {Data: message, Charset: 'UTF-8'}
+                            },
+                            Subject: {
+                                Data: 'successfully subscribed to Event: ' + info.name,
+                                Charset: 'UTF-8'
+                            }
+                        },
+                        Source: "disasterwatch@remotepixel.ca"
+                    };
+
+                // Send the email
+                sendMail(params, function(err, res) {
+                    if (err) {
+                        context.succeed("disasterEvent Added to database but couldn't send Email");
+                    }
+                    context.succeed("disasterEvent Added to database and mail sent");
+                });
+            }
         });
     });
 };
@@ -57,7 +104,38 @@ module.exports.update = function(event, context) {
                 db.close();
                 console.log("Successfully updated disasterEvent");
                 //Confirmation MAIL to "event.mailTO"
-                context.succeed("Successfully updated disasterEvent");
+                // context.succeed("Successfully updated disasterEvent");
+
+                var mailTemplate = fs.readFileSync('./templates/subscribe.html', 'utf8'),
+                    info = {
+                        name: geojsonFeature.properties.name,
+                        uuid: geojsonFeature.properties.uuid,
+                        mail: mail.mail
+                    },
+                    message = mark.up(mailTemplate, info),
+                    params = {
+                        Destination: {
+                            ToAddresses: [info.mail]
+                        },
+                        Message: {
+                            Body: {
+                                Html: {Data: message, Charset: 'UTF-8'}
+                            },
+                            Subject: {
+                                Data: 'successfully subscribed to Event: ' + info.name,
+                                Charset: 'UTF-8'
+                            }
+                        },
+                        Source: "disasterwatch@remotepixel.ca"
+                    };
+
+                // Send the email
+                sendMail(params, function(err, res){
+                    if (err) {
+                        context.succeed("disasterEvent updated but couldn't send Email");
+                    }
+                    context.succeed("disasterEvent updated and mail sent");
+                });
             });
         } else {
             col.updateOne({uuid: uuid}, {$set: {'feature': geojsonFeature}}, {upsert: true}, function(err, doc) {
@@ -98,10 +176,45 @@ module.exports.subscribe = function(event, context) {
                 console.log("Couldn't subscribe to disasterEvent");
                 context.fail(err);
             }
-            db.close();
+
             console.log("Successfully subscribed to disasterEvent");
-            //Confirmation MAIL to "event.mailTO"
-            context.succeed("Successfully subscribed to disasterEvent");
+
+            setTimeout(function() {
+              // Fetch the document that we modified
+              col.findOne({uuid: event.body.uuid}, function(err, item) {
+                  var mailTemplate = fs.readFileSync('./templates/subscribe.html', 'utf8'),
+                      info = {
+                          name: item.feature.properties.name,
+                          uuid: item.feature.properties.uuid,
+                          mail: event.body.mail.mail
+                      },
+                      message = mark.up(mailTemplate, info),
+                      params = {
+                          Destination: {
+                              ToAddresses: [info.mail]
+                          },
+                          Message: {
+                              Body: {
+                                  Html: {Data: message, Charset: 'UTF-8'}
+                              },
+                              Subject: {
+                                  Data: 'successfully subscribed to Event: ' + info.name,
+                                  Charset: 'UTF-8'
+                              }
+                          },
+                          Source: "disasterwatch@remotepixel.ca"
+                      };
+
+                  // Send the email
+                  sendMail(params, function(err, res){
+                      if (err) {
+                          context.succeed("Couldn't subscribe to disasterEvent");
+                      }
+                      context.succeed("Successfully subscribed to disasterEvent");
+                  });
+                db.close();
+              });
+            }, 1000);
         });
     });
 };
@@ -116,10 +229,42 @@ module.exports.unsubscribe = function(event, context) {
                 console.log("Couldn't unsubscribe to disasterEvent");
                 context.fail(err);
             }
-            db.close();
-            //Confirmation MAIL to "event.mailTO"
             console.log("Successfully unsubscribe to disasterEvent");
-            context.succeed("Successfully unsubscribe to disasterEvent");
+            setTimeout(function() {
+              // Fetch the document that we modified
+              col.findOne({uuid: event.body.uuid}, function(err, item) {
+                  var mailTemplate = fs.readFileSync('./templates/unsubscribe.html', 'utf8'),
+                      info = {
+                          name: item.feature.properties.name,
+                          uuid: item.feature.properties.uuid
+                      },
+                      message = mark.up(mailTemplate, info),
+                      params = {
+                          Destination: {
+                              ToAddresses: [event.body.mail]
+                          },
+                          Message: {
+                              Body: {
+                                  Html: {Data: message, Charset: 'UTF-8'}
+                              },
+                              Subject: {
+                                  Data: 'successfully unsubscribed to Event: ' + info.name,
+                                  Charset: 'UTF-8'
+                              }
+                          },
+                          Source: "disasterwatch@remotepixel.ca"
+                      };
+
+                  // Send the email
+                  sendMail(params, function(err, res){
+                      if (err) {
+                          context.succeed("Couldn't unsubscribe to disasterEvent");
+                      }
+                      context.succeed("Successfully unsubscribe to disasterEvent");
+                  });
+                db.close();
+              });
+            }, 1000);
         });
     });
 };
