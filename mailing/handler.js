fbd8ad00e8b3848a73cb2c476ff1f21daefe4035
vincentsarago <vincent.sarago@gmail.com>
2016-10-17 00:18:06 -0400

    do not judge me, to many hacks in this code

---
+++ mailing/handler.js
@@ -0,0 +1,185 @@
+var fs = require('fs');
+var d3 = require('d3-queue');
+var moment = require('moment');
+var mark = require('markup-js');
+var aws = require('aws-sdk');
+
+var db = require('./lib/dbconnect.js');
+var satapi = require('./lib/sat-api.js');
+var scihubapi = require('./lib/scihub_sentinel1.js');
+
+var ses = new aws.SES({region: 'us-west-2'});
+
+function sortScenes(a, b) {
+    return Date.parse(b.date) - Date.parse(a.date);
+}
+
+
+var mailTemplate = fs.readFileSync('./templates/images_update.html', 'utf8');
+
+
+function construcMailInfo(prop, mail, images) {
+
+    var imgTemplate = '<td valign="top" width="280" class="leftColumnContent">' +
+                        '<table border="0" cellpadding="20" cellspacing="0" width="100%">' +
+                            '<tr><td valign="top">' +
+                                '<img src="{{browseURL}}" style="max-width:260px;"/>' +
+                                '<div>' +
+                                    '<strong>Satellite:</strong> {{satellite}}<br />' +
+                                    '<strong>Date:</strong> {{date}}<br />' +
+                                    '<strong>Cloud %:</strong> {{cloud}}<br />' +
+                                    '<a href="{{downloadURL}}" target="_blank">link</a>' +
+                                '</div></td>' +
+                            '</tr></table></td>';
+
+    var info = {
+        nbImages : images.length,
+        name: prop.name,
+        uuid: prop.uuid,
+        mail: mail.mail,
+        imageslist : '<tr>'
+    };
+
+    for(var jj = 0; jj < images.length; jj++) {
+        var imginfo = {
+
+            browseURL: images[jj].browseURL,
+            satellite: images[jj].sat,
+            date: images[jj].date,
+            cloud: (images[jj].sat !== 'sentinel1')? images[jj].cloud : '- ',
+            downloadURL: images[jj].downURL
+        }
+
+        var imgBlock = mark.up(imgTemplate, imginfo);
+        info.imageslist += imgBlock;
+        if (jj % 2 === 1) info.imageslist += '</tr><tr>';
+    }
+
+    info.imageslist += '</tr>';
+
+    return info;
+}
+
+function sendMail (content, callback) {
+    ses.sendEmail(content, function (err, data) {
+        if (err) {
+            console.log('Internal Error: The email could not be sent.');
+        } else {
+            console.log('The email was successfully sent to ' + content.Destination.ToAddresses[0]);
+            return callback(null, 'The email was successfully sent to ' + content.Destination.ToAddresses[0]);
+        }
+    });
+}
+
+function getImagery(doc, cb){
+
+    var q = d3.queue();
+    q.defer(scihubapi.getS1Images, doc.feature, {'dateStart': moment(doc.feature.properties.images.sentinel1).utc().add(1, 'days').format('YYYY-MM-DD')});
+    q.defer(satapi.getL8Images, doc.feature, {'dateStart': moment(doc.feature.properties.images.landsat8).utc().add(1, 'days').format('YYYY-MM-DD')});
+    q.defer(satapi.getS2Images, doc.feature, {'dateStart': moment(doc.feature.properties.images.sentinel2).utc().add(1, 'days').format('YYYY-MM-DD')});
+    q.awaitAll(function(error, results) {
+        var images = [];
+        if (results.length !== 0) {
+            for (var j = 0; j <results.length; j++) {
+                if (results[j]) {
+                    images = images.concat(results[j]);
+                }
+            }
+        }
+        images.sort(sortScenes);
+
+        if (images.length === 0) {
+            console.log('No new Image for event: ' + doc.feature.properties.name);
+            return cb(null, 1)
+        } else {
+            console.log(images.length + ' new Image for event: ' + doc.feature.properties.name);
+
+            var s1img = images.filter(function(e){
+                return (e.sat === 'sentinel1');
+            });
+
+            var s2img = images.filter(function(e){
+                return (e.sat === 'sentinel2');
+            });
+
+            var l8img = images.filter(function(e){
+                return (e.sat === 'landsat8');
+            });
+
+            // update database
+            if (s1img.length !== 0) {
+                doc.feature.properties.images.sentinel1 = s1img[0].date;
+            }
+            if (s2img.length !== 0) {
+                doc.feature.properties.images.sentinel2 = s2img[0].date;
+            }
+            if (l8img.length !== 0) {
+                doc.feature.properties.images.landsat8 = l8img[0].date;
+            }
+
+            var q2 = d3.queue();
+            for(var ii = 0; ii < doc.mail.length; ii++) {
+
+                var img2send = images.filter(function(e){
+                    return (doc.mail[ii].satellite.indexOf(e.sat) > -1);
+                });
+
+                if (img2send.length === 0) {continue}
+
+                var info = construcMailInfo(doc.feature.properties, doc.mail[ii], img2send),
+                    message = mark.up(mailTemplate, info),
+                    params = {
+                        Destination: {
+                            // ToAddresses: ["contact@remotepixel.ca"]
+                            ToAddresses: [doc.mail[ii].mail]
+                        },
+                        Message: {
+                            Body: {
+                                Html: {Data: message, Charset: 'UTF-8'}
+                            },
+                            Subject: {
+                                Data: 'New Images Available for Event: ' + info.name,
+                                Charset: 'UTF-8'
+                            }
+                        },
+                        Source: "disasterwatch@remotepixel.ca"
+                    };
+
+                    // Send the email
+                    q2.defer(sendMail, params);
+            }
+
+            q2.awaitAll(function(error, results) {
+                db.imgDateUpdate(doc.uuid, doc.feature.properties.images, function(err, res) {
+                    return cb(null, 1)
+                });
+            });
+        }
+    });
+}
+
+module.exports.checkImagery = function (event, context) {
+    db.getDB(function (err, docs) {
+        if (err) {
+            context.fail("Cannot connect to db");
+        }
+        var count = 0;
+
+        for(var i = 0; i < docs.length; i++) {
+
+            var doc = docs[i];
+
+            getImagery(doc, function(err, res){
+
+                count += 1
+                if (count ===  docs.length){
+                    context.succeed("Done");
+                }
+            })
+
+        }
+        if (docs.length === 0) {
+            context.succeed("Done");
+        }
+    });
+}
---
+++ mailing/lib/sat-api.js
@@ -0,0 +1,110 @@
+var request = require('request');
+var moment = require('moment');
+var config = require('./config');
+
+function zeroPad(n, c) {
+    'use strict';
+    var s = String(n);
+    if (s.length < c) {
+        return zeroPad('0' + n, c);
+    }
+    return s;
+}
+
+module.exports.getL8Images = function(feature, options, callback) {
+    if (options && options.hasOwnProperty('dateStart')) {
+        dateStart = moment(options.dateStart).utc().format("YYYY-MM-DD");
+    } else {
+        dateStart = '2016-01-01';
+    }
+
+    var results = [],
+        query = {
+                intersects: feature,
+                satellite_name: 'landsat',
+                date_from: dateStart,
+                date_to: moment.utc().format('YYYY-MM-DD'),
+                limit: 2000
+            };
+
+    request({
+        url: 'https://api.developmentseed.org/satellites', //URL to hit
+        method: 'POST',
+        json: query
+    }, function(err, response, data){
+        if (!err && response.statusCode == 200) {
+            if (data.meta.found !== 0) {
+                var i;
+                for (i = 0; i < data.results.length; i += 1) {
+                    var scene = {};
+                    scene.date = data.results[i].date;
+                    scene.cloud = data.results[i].cloud_coverage;
+                    scene.sceneID = data.results[i].scene_id;
+                    scene.sat = 'landsat8';
+                    scene.path = data.results[i].path.toString();
+                    scene.row = data.results[i].row.toString();
+                    scene.grid = data.results[i].path + '/' + data.results[i].row;
+                    scene.usgsURL = data.results[i].cartURL;
+                    scene.browseURL = data.results[i].browseURL.replace('http://', "https://");
+                    scene.AWSurl = 'http://landsat-pds.s3.amazonaws.com/L8/' + zeroPad(data.results[i].path, 3) + '/' + zeroPad(data.results[i].row, 3) + '/' + data.results[i].sceneID + '/';
+                    scene.sumAWSurl = 'https://landsatonaws.com/L8/' + zeroPad(data.results[i].path, 3) + '/' + zeroPad(data.results[i].row, 3) + '/' + data.results[i].sceneID;
+                    scene.downURL = 'https://landsatonaws.com/L8/' + zeroPad(data.results[i].path, 3) + '/' + zeroPad(data.results[i].row, 3) + '/' + data.results[i].sceneID;
+                    results.push(scene);
+                }
+            }
+            return callback(null, results);
+        } else {
+            return callback(err);
+        }
+    })
+}
+
+module.exports.getS2Images = function(feature, options, callback) {
+
+    if (options && options.hasOwnProperty('dateStart')) {
+        dateStart = moment(options.dateStart).utc().format("YYYY-MM-DD");
+    } else {
+        dateStart = '2016-01-01';
+    }
+
+    var results = [],
+        query = {
+                intersects: feature,
+                satellite_name: 'sentinel',
+                date_from: dateStart,
+                date_to: moment.utc().format('YYYY-MM-DD'),
+                limit: 2000
+            };
+
+    request({
+        url: 'https://api.developmentseed.org/satellites', //URL to hit
+        method: 'POST',
+        json: query
+    }, function(err, response, data){
+        if (!err && response.statusCode == 200) {
+            if (data.meta.found !== 0) {
+                var i;
+                for (i = 0; i < data.results.length; i += 1) {
+                    var scene = {};
+                    scene.date = data.results[i].date;
+                    scene.cloud = data.results[i].cloud_coverage;
+                    scene.sceneID = data.results[i].scene_id;
+                    scene.sat = 'sentinel2';
+                    scene.utm_zone = data.results[i].utm_zone.toString();
+                    scene.grid_square = data.results[i].grid_square;
+                    scene.coverage = data.results[i].data_coverage_percentage;
+                    scene.latitude_band = data.results[i].latitude_band;
+                    scene.browseURL = data.results[i].thumbnail.replace('.jp2', ".jpg");
+                    scene.path = data.results[i].aws_path.replace('tiles', "#tiles");
+                    scene.AWSurl = 'http://sentinel-s2-l1c.s3-website.eu-central-1.amazonaws.com/' + scene.path + '/';
+                    scene.downURL = 'http://sentinel-s2-l1c.s3-website.eu-central-1.amazonaws.com/' + scene.path + '/';
+                    scene.grid = scene.utm_zone + scene.latitude_band + scene.grid_square;
+                    results.push(scene);
+                }
+            }
+            return callback(null, results);
+        } else {
+            return callback(err);
+        }
+    })
+}
